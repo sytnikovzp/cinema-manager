ebc7926248d43fbc1c5449d8378643cd2ef9ca02
alex <sytnikov.zp@gmail.com>
2024-05-26 13:42:45 +0300

    Implement HomePage & Carousel

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,41 @@
+import Carousel from 'react-material-ui-carousel';
+import { Box } from '@mui/material';
+import { posters } from '../../constants';
+
+const styles = {
+  imgContainerStyle: {
+    position: 'relative',
+    maxWidth: '100%',
+    height: '70vh',
+    overflow: 'hidden',
+    color: 'red',
+  },
+  imgStyle: {
+    borderRadius: '15px',
+    padding: '5px',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translate(-50%, 0)',
+    objectFit: 'contain',
+  },
+};
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <Carousel>
+        {posters.map((poster) => {
+          return (
+            <Box key={poster.id} styles={styles.imgContainerStyle}>
+              <img style={styles.imgStyle} src={poster.url} alt={poster.alt} />
+            </Box>
+          );
+        })}
+      </Carousel>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/constants/index.js
@@ -0,0 +1,37 @@
+export const posters = [
+  {
+    id: 1,
+    url: 'https://pluggedin.ru/images/upload/1655926482.jpg',
+    alt: 'poster',
+  },
+  {
+    id: 2,
+    url: 'https://www.combook.ru/imgrab/0068/9785041165246.jpg',
+    alt: 'poster',
+  },
+  {
+    id: 3,
+    url: 'https://www.themoviedb.org/t/p/original/9AhKm1JP67ZvuUTCmYs3SVlHm0c.jpg',
+    alt: 'poster',
+  },
+  {
+    id: 4,
+    url: 'https://xage.ru/media/uploads/2017/08/thor_ragnarok_04_01.jpg',
+    alt: 'poster',
+  },
+  {
+    id: 5,
+    url: 'https://poster4.me/wp-content/uploads/2020/01/mstiteli_9.jpg',
+    alt: 'poster',
+  },
+  {
+    id: 6,
+    url: 'https://xage.ru/media/uploads/2008/2/posteryi-luchshih-filmov-poluchivshih-oskar/posteryi-luchshih-filmov-poluchivshih-oskar_1.jpg',
+    alt: 'poster',
+  },
+  {
+    id: 7,
+    url: 'https://megaobzor.com/uploads/stories/66059/poster40933_1.jpg',
+    alt: 'poster',
+  },
+];
