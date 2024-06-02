91a542b10727d6081d6c98979eda2ea8eca3202c
alex <sytnikov.zp@gmail.com>
2024-06-02 23:42:46 +0300

    Set dark theme to App

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
 // =============================
 import App from './App.jsx';
 import store from './store';
 import './index.css';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
