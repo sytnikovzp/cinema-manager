04cf591d0647cc10964cfd27036bba4771fac016
alex <sytnikov.zp@gmail.com>
2024-06-03 20:34:53 +0300

    Refactoring ActorsItem components

--- src/components/Actors/ActorsItem.jsx
+++ src/components/Actors/ActorsItem.jsx
@@ -1,11 +1,32 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 import { getActorById } from '../../store/slices/actorsSlice';
 
-function ActorsItem({ id }) {
-  console.log(id);
+function ActorsItem() {
+  const dispatch = useDispatch();
 
-  return <div>{`${fullName}, ${nationality}`}</div>;
+  const { id } = useParams();
+
+  const actors = useSelector((state) => state.actorsList.actors);
+
+  const { fullName, birthYear, nationality, image } = actors.find(
+    (actor) => actor.id === Number(id)
+  );
+
+  console.log(fullName, birthYear, nationality, image);
+
+  useEffect(() => {
+    dispatch(getActorById(id));
+  }, [dispatch, id]);
+
+  return (
+    <ul className='actors-container'>
+      <p>
+        {fullName}, {birthYear} ,{nationality}, {image}{' '}
+      </p>
+    </ul>
+  );
 }
 
 export default ActorsItem;
