deb86e1a42a1c35415c3fc5300df5383c4ed56cb
alex <sytnikov.zp@gmail.com>
2024-06-05 14:39:22 +0300

    setStatus 'null' instead of 'pending'

--- src/services/reducer-service.js
+++ src/services/reducer-service.js
@@ -1,9 +1,9 @@
-export const setError = (state, {payload}) => {
-    state.error = payload;
-    state.status = 'rejected';
-}
+export const setError = (state, { payload }) => {
+  state.error = payload;
+  state.status = 'rejected';
+};
 
 export const setStatus = (state) => {
-    state.error = null;
-    state.status = 'pending';
-}
+  state.error = null;
+  state.status = null;
+};
