e83b468ee26cfecb50045451b0d144be6a66b68a
alex <sytnikov.zp@gmail.com>
2024-06-28 22:58:18 +0300

    Display last seven movies poster at HomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,20 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// =============================================
 import Carousel from 'react-material-ui-carousel';
 // =============================================
 import Box from '@mui/material/Box';
 // =============================================
 import { carouselStyles } from '../../services/styleService';
-import { posters } from '../../constants';
+// =============================================
+import { getAllMovies } from '../../store/slices/moviesSlice';
 
 function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllMovies());
+  }, [dispatch]);
+
+  const movies = useSelector((state) => state.moviesList.movies);
+
+  const lastSevenMovies = movies.slice(-7);
+
   return (
     <>
       <Carousel stopAutoPlayOnHover>
-        {posters.map((poster) => {
+        {lastSevenMovies.map((movie) => {
           return (
-            <Box key={poster.id} style={carouselStyles.imgContainerStyle}>
+            <Box key={movie.id} style={carouselStyles.imgContainerStyle}>
               <img
-                src={poster.url}
-                alt={poster.alt}
+                src={movie.poster}
+                alt={movie.title}
                 style={carouselStyles.imgStyle}
               />
             </Box>
--- src/constants/index.js
+++ src/constants/index.js
@@ -48,44 +48,6 @@ export const emptyStudio = {
   about: '',
 };
 
-export const posters = [
-  {
-    id: 1,
-    url: 'https://www.tallengestore.com/cdn/shop/products/HouseOfTheDragon_GoT_-TVShowPoster2_a69da114-1374-4a54-8a66-bf67c1922b9e.jpg',
-    alt: 'poster',
-  },
-  {
-    id: 2,
-    url: 'https://i.pinimg.com/736x/6b/2f/41/6b2f4114cafe3a3bc0ab845602c3cea6.jpg',
-    alt: 'poster',
-  },
-  {
-    id: 3,
-    url: 'https://www.themoviedb.org/t/p/original/9AhKm1JP67ZvuUTCmYs3SVlHm0c.jpg',
-    alt: 'poster',
-  },
-  {
-    id: 4,
-    url: 'https://xage.ru/media/uploads/2017/08/thor_ragnarok_04_01.jpg',
-    alt: 'poster',
-  },
-  {
-    id: 5,
-    url: 'https://m.media-amazon.com/images/I/811xdZfsUqL._AC_UF1000,1000_QL80_.jpg',
-    alt: 'poster',
-  },
-  {
-    id: 6,
-    url: 'https://m.media-amazon.com/images/I/81EBp0vOZZL._AC_SY879_.jpg',
-    alt: 'poster',
-  },
-  {
-    id: 7,
-    url: 'https://m.media-amazon.com/images/I/915RviLDi3L._AC_UF894,1000_QL80_.jpg',
-    alt: 'poster',
-  },
-];
-
 export const genres = [
   {
     id: 1,
