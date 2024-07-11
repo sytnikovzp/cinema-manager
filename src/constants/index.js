e373e270486340ca1baaec8ac117dfe52319177c
alex <sytnikov.zp@gmail.com>
2024-07-11 16:53:15 +0300

    Add routes (/api) to json-server

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:5000';
-// export const BASE_URL = 'https://sytnikov.site:5000';
+export const BASE_URL = 'http://localhost:5000/api';
+// export const BASE_URL = 'http://sytnikov.site:5000/api';
 
 export const ACTORS_SLICE_NAME = 'actors';
 export const DIRECTORS_SLICE_NAME = 'directors';
