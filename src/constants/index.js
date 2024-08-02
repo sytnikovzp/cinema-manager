94b141ce0b47109531cb39b1db815ff162caf155
alex <sytnikov.zp@gmail.com>
2024-08-02 22:29:36 +0300

    The client part is separated from the json-server
    https://github.com/sytnikovzp/Cinema-manager-json-server (branch 'local')

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'http://localhost:5000/api';
 // export const BASE_URL = 'https://sytnikov.site:5000/api';
 
 export const ACTORS_SLICE_NAME = 'actors';
