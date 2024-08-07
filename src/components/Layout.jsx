fc27298d8b648955e441312495e7cc72d65ce2cd
alex <sytnikov.zp@gmail.com>
2024-08-07 11:59:35 +0300

    Fixed adaptivity for mobile devices (Layout)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -30,21 +30,21 @@ function Layout() {
               : theme.palette.grey[700],
         }}
       >
-        <Grid item lg={12} md={12} xl={12} sm={12} xs={12}>
+        <Grid item xs={12}>
           <Header />
         </Grid>
         <Grid container sx={{ mt: '1rem', mb: '1rem', flex: 1 }}>
-          <Grid item lg={2} md={2} xl={2} sm={2} xs={2}>
+          <Grid item lg={2} md={2} sm={12} xs={12}>
             <NavBar />
           </Grid>
-          <Grid item lg={6} md={6} xl={6} sm={6} xs={6}>
+          <Grid item lg={6} md={6} sm={12} xs={12}>
             <Outlet />
           </Grid>
-          <Grid item lg={4} md={4} xl={4} sm={4} xs={4}>
+          <Grid item lg={4} md={4} sm={12} xs={12}>
             <CinemaService />
           </Grid>
         </Grid>
-        <Grid item lg={12} md={12} xl={12} sm={12} xs={12}>
+        <Grid item xs={12}>
           <Footer />
         </Grid>
       </Grid>
