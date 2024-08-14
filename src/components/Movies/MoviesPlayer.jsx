1ea6fd1d9d947ba7c5da7b96e1ba1972f5bee0ac
alex <sytnikov.zp@gmail.com>
2024-08-14 15:20:57 +0300

    Fix README: added changelog by history commit

--- src/components/Movies/MoviesPlayer.jsx
+++ src/components/Movies/MoviesPlayer.jsx
@@ -13,6 +13,7 @@ function MoviesPlayer({ trailer }) {
           url={trailer}
           light
           width='100%'
+          height='55vh'
           config={{
             youtube: {
               playerVars: {
